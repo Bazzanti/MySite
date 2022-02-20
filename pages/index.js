ceb88dd74917e7fb870a4823ef6a3dd0f0c05fdf
michael.bazzanti <michael.bazzanti-ext@prada.com>
2022-02-20 20:06:02 +0100

    column + image

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
+import mypic from '../public/profile.jpg'
 
 export default function Home() {
 
@@ -32,66 +33,49 @@ export default function Home() {
       <div className={styles.content} >
         <main className={styles.main} >
           <section id="me">
+            <div className={styles.row}>
+              <div className={styles.column}>
+                <h4 className={styles.smallTitle}>
+                  Hi, my name is
+                </h4>
 
-            <h4 className={styles.smallTitle}>
-              Hi, my name is
-            </h4>
+                <div className={styles.wrappedAnimationText }>
+                  <span data-content="Michael Bazzanti" aria-hidden="true" className={styles.animationText }>
+                  </span>
+                  Michael Bazzanti
+                </div>
 
-            <div className={styles.wrappedAnimationText }>
-              <span data-content="Michael Bazzanti" aria-hidden="true" className={styles.animationText }>
-              </span>
-              Michael Bazzanti
+                <h4 className={styles.smallTitle}>
+                  and I am a 
+                </h4>
+
+                <div className={styles.wrappedAnimationText }>
+                  <span data-content="Fullstack_Developer" aria-hidden="true" className={styles.animationText }>
+                  </span>
+                  Fullstack_Developer
+                </div>
+
+
+                <p className={styles.description}>
+                  I love discovering new {' '} 
+                  <code className={styles.code}>technologies</code> and build fantastic {' '} 
+                  <code className={styles.code}>digital</code> experiences.
+                </p>
+              </div>
+              <div className={styles.column + ' ' + styles.center}>
+                <div className={styles.image}>
+                  <Image
+                    src={mypic}
+                    alt="Picture of the author"
+                    layout="responsive" 
+                    objectFit="cover" 
+                    placeholder="blur" 
+                  />
+                </div>
+              </div>
             </div>
-
-            <h4 className={styles.smallTitle}>
-              and I am a 
-            </h4>
-
-            <div className={styles.wrappedAnimationText }>
-              <span data-content="Fullstack_Developer" aria-hidden="true" className={styles.animationText }>
-              </span>
-              Fullstack_Developer
-            </div>
-
-
-            <p className={styles.description}>
-              I love discovering new {' '} 
-              <code className={styles.code}>technologies</code> and build fantastic {' '} 
-              <code className={styles.code}>digital</code> experiences.
-            </p>
           </section> 
 
-    {/* 
-
-            <div className={styles.grid}>
-              <a href="https://nextjs.org/docs" className={styles.card}>
-                <h2>Documentation &rarr;</h2>
-                <p>Find in-depth information about Next.js features and API.</p>
-              </a>
-
-              <a href="https://nextjs.org/learn" className={styles.card}>
-                <h2>Learn &rarr;</h2>
-                <p>Learn about Next.js in an interactive course with quizzes!</p>
-              </a>
-
-              <a
-                href="https://github.com/vercel/next.js/tree/canary/examples"
-                className={styles.card}
-              >
-                <h2>Examples &rarr;</h2>
-                <p>Discover and deploy boilerplate example Next.js projects.</p>
-              </a>
-
-              <a
-                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                className={styles.card}
-              >
-                <h2>Deploy &rarr;</h2>
-                <p>
-                  Instantly deploy your Next.js site to a public URL with Vercel.
-                </p>
-              </a>
-            </div> */}
         </main>
 
         <footer className={styles.footer}>
