8a1faffb8b105a10d083488be419253e1ab1b524
michael.bazzanti <michael.bazzanti-ext@prada.com>
2022-03-06 19:41:31 +0100

    structure folders

--- pages/index.js
+++ pages/index.js
@@ -1,97 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
-import mypic from '../public/profile.JPG'
+import mypic from '../public/profilee.jpg'
 import {AiOutlineMail, AiOutlineLinkedin} from 'react-icons/ai';
+import Developer from './components/developer/developer';
 
 export default function Home() {
 
-
-
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Michael Bazzanti</title>
-        <meta name="description" content="Michael Bazzanti website" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <><Developer></Developer></>
 
-      <header className={styles.headerBar}>
-        <nav className={styles.navbar}>
-          <ul className={styles.ulbar}>
-            <li><a href="#me"> 01. Me </a></li>
-          </ul>
-        </nav>
-      </header>
-      
-      <div orientation="left">
-        <ul className={styles.verticalList}>
-          <li className={styles.listIcon}> 
-            <a href="mailto:michael6993@msn.com">  <AiOutlineMail /> </a>
-          </li>
-          <li className={styles.listIcon}> 
-            <a href="https://www.linkedin.com/in/michael-bazzanti-589221129/"> <AiOutlineLinkedin /> </a>
-          </li>
-        </ul>
-      </div>
-
-      <div className={styles.content} >
-        <main className={styles.main} >
-          <section id="me">
-            <div className={styles.row}>
-              <div className={styles.column}>
-                <h4 className={styles.smallTitle}>
-                  Hi, my name is
-                </h4>
-
-                <div className={styles.wrappedAnimationText }>
-                  <span data-content="Michael Bazzanti" aria-hidden="true" className={styles.animationText }>
-                  </span>
-                  Michael Bazzanti
-                </div>
-
-                <h4 className={styles.smallTitle}>
-                  and I am a 
-                </h4>
-
-                <div className={styles.wrappedAnimationText }>
-                  <span data-content="Software Developer" aria-hidden="true" className={styles.animationText }>
-                  </span>
-                  Software Developer
-                </div>
-
-
-                <p className={styles.description}>
-                  I love discovering new {' '} 
-                  <code className={styles.code}>technologies</code> and build fantastic {' '} 
-                  <code className={styles.code}>digital</code> experiences.
-                </p>
-              </div>
-              <div className={styles.column + ' ' + styles.center}>
-                <div className={styles.image}>
-                  <Image
-                    src={mypic}
-                    alt="Picture of the author"
-                    layout="responsive" 
-                    objectFit="cover" 
-                    placeholder="blur" 
-                  />
-                </div>
-              </div>
-            </div>
-          </section> 
-          <section id="me">
-          </section>
-
-        </main>
-
-        <footer className={styles.footer}>
-            Designed and built by Michael Bazzanti
-            {/* <span className={styles.logo}>
-              <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-            </span> */}
-        </footer>
-      </div>
-    </div>
   )
 }
